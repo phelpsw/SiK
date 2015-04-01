04b989aee3bc6bff726efde3156c155485850bd4
Luke Hovington <lukeh@hovo.id.au>
2015-04-01 10:31:21 +1000

    Modify for AES support and bugs for 900p

--- Firmware/include/board_rfd900pe.h
+++ Firmware/include/board_rfd900pe.h
@@ -59,79 +59,12 @@
 /// it is a flow control input to the SiK radio firmware.
 
 
-#ifndef _BOARD_RFD900P
-#define _BOARD_RFD900P
+#ifndef _BOARD_RFD900PE
+#define _BOARD_RFD900PE
 
-#include <compiler_defs.h>
-#include <Si1020_defs.h>
+#include "board_rfd900p.h"
 
-// Ensure that the BoardID has the upper most bit set
-// This tells the tool chain we are dealing with a CPU_SI1030 device
-#define BOARD_ID	 0x80 | 0x02
-#define BOARD_NAME	"RFD900P"
-#define CPU_SI1030
+#define BOARD_rfd900p
+#define INCLUDE_AES
 
-#define BOARD_MINTXPOWER 0		// Minimum transmit power level
-#define BOARD_MAXTXPOWER 30		// Maximum transmit power level
-
-//#define WATCH_DOG_ENABLE
-
-// GPIO definitions (not exported)
-SBIT(LED_RED,      SFR_P3, 6);
-SBIT(LED_GREEN,    SFR_P3, 7);
-SBIT(PIN_CONFIG,   SFR_P0, 2);
-SBIT(PIN_ENABLE,   SFR_P0, 3);
-SBIT(PA_ENABLE,    SFR_P2, 5);         // Power Amplifier Enable
-
-
-// Signal polarity definitions
-#define LED_ON		1				// LED Sense inverted when compared to HM_TRP
-#define LED_OFF		0
-#define BUTTON_ACTIVE	0
-
-// UI definitions
-#define LED_BOOTLOADER	LED_RED
-#define LED_RADIO		LED_GREEN
-#define LED_ACTIVITY	LED_RED
-#define BUTTON_BOOTLOAD	PIN_CONFIG
-
-// Serial flow control
-#define SERIAL_RTS	PIN_ENABLE	// always an input
-#define SERIAL_CTS	PIN_CONFIG	// input in bootloader, output in app
-
-// board-specific hardware config
-#define HW_INIT							\
-	do { \
-		SFRPAGE  = CONFIG_PAGE; \
-		P3MDOUT |= 0x40;		/* Led Red */ \
-		P3DRV   |= 0x40;		/* Led Red */ \
-		SFRPAGE  = LEGACY_PAGE;	\
-		/* Setup Timers */ \
-		TMOD	 = (TMOD & ~0xf0) | 0x20; /* TMOD: timer 1 in 8-bit autoreload */ \
-		TR1		 = 1;			/* START Timer1 */ \
-		TI0		 = 1;			/* Indicate TX0 ready */ \
-		/* INT0 is the radio interrupt, on P0.1 */ \
-		IT01CF	 = (IT01CF & 0xf) | 0x01;\
-		IT0		 = 0;			/* INT0 level triggered */ \
-		P2		 = 0xFF;		/* P2 bug fix for SDCC and Raisonance*/ \
-	} while(0)
-
-// application/board-specific hardware config
-#define HW_INIT_APPLICATION					\
-	do {							\
-		SFRPAGE	 =  CONFIG_PAGE;			\
-		P0DRV	|=    0x04;		/* CTS */	\
-		SFRPAGE	 =  LEGACY_PAGE;			\
-	} while(0)
-
-// Radio Definitions
-
-#define EZRADIOPRO_OSC_CAP_VALUE 0xB6 // Measured on RFD900 V1.1
-#define ENABLE_RFD900_SWITCH 1        // Define RF switches on the module (V1.1 are V1.2 the same)
-#define RFD900_DIVERSITY 1            // Enable/Disable diversity on RFD900 (V1.1 are V1.2 the same)
-#define RFD900_INT_TX_POW 4           // TX power feeding into the amp
-#define TEMP_OFFSET 45                // Use the internal offset register with this extra cal offset
-SBIT(IRQ,  SFR_P0, 1);                // Connection within RFD900 module, P0.1 is connected to nIRQ
-SBIT(NSS1, SFR_P2, 3);                // SI1020 Internal Connection
-
-#endif // _BOARD_RFD900P
+#endif // _BOARD_RFD900PE
